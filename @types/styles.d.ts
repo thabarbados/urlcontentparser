59e60dc29df73fab2156d0cfc864dce4be8577d6
thabarbados <thabarbados@gmail.com>
2020-03-22 19:29:14 +0700

    core: types folder and files

---
+++ @types/styles.d.ts
@@ -0,0 +1,9 @@
+declare module '*.scss' {
+  const content: { [className: string]: string };
+  export default content;
+}
+
+declare module '*.scss?module' {
+  const content: { [className: string]: string };
+  export default content;
+}
